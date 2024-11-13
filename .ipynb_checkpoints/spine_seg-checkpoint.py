25b9b3f7c748fadb9961b7a4a842dbbe3622ab2f
cgartland2 <cliodhna.gartland@ucdconnect.ie>
2024-11-13 03:36:54 +0000

    file path

--- .ipynb_checkpoints/spine_seg-checkpoint.py
+++ .ipynb_checkpoints/spine_seg-checkpoint.py
@@ -21,6 +21,7 @@ from torchvision import datasets, transforms, models
 
 from sam2.build_sam import build_sam2
 from sam2.sam2_image_predictor import SAM2ImagePredictor
+from pydicom import dcmread
 
 
 def main():
