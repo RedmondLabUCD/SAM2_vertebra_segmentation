b1c4e53efa7d6b2b40ebe61a1d8075b757488ac6
cgartland2 <cliodhna.gartland@ucdconnect.ie>
2024-11-13 02:22:00 +0000

    first commit

---
+++ spine_seg.py
@@ -0,0 +1,86 @@
+from __future__ import print_function
+import os
+import time
+import json
+import argparse
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+from sklearn.model_selection import KFold
+import shutil
+from glob import glob
+import gc
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, ConcatDataset, random_split, Subset
+from torchvision import datasets, transforms, models
+
+import models
+from utils.earlystopping import EarlyStopping
+from utils import datasets
+from torchvision.datasets.utils import list_files
+from utils.params import Params
+from utils.plotting import plot_training
+from utils.train_progress_tools import run_train_generator, track_running_average_loss, monitor_progress
+import utils.eval_metrics as e_metric
+from utils.data_prep import final_mean_and_std
+from sam2.build_sam import build_sam2
+from sam2.sam2_image_predictor import SAM2ImagePredictor
+
+
+
+def main():
+
+    sam2_checkpoint = "//trinity/home/r094879/repositories/sam2/checkpoints/sam2.1_hiera_large.pt"
+    model_cfg = "//trinity/home/r094879/repositories/sam2/sam2/configs/sam2.1/sam2.1_hiera_l.yaml"
+    
+    sam2_model = build_sam2(model_cfg, sam2_checkpoint)
+
+    predictor = SAM2ImagePredictor(sam2_model)
+
+    csv_file = '//data/scratch/r094879/data/annotations/annotations.csv' 
+    df = pd.read_csv(csv_file)
+
+    image_dir = '//data/scratch/r094879/data/images'
+    output_dir = '//data/scratch/r094879/data/sam_seg'
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for index, row in df.iterrows():
+        image_name = row['image']  # Get the DICOM image name from the 'image' column
+        output_file_path = os.path.join(output_dir,image_name+'.png')
+
+        img = dcmread(os.path.join(image_dir,image_name+".dcm"))
+        img_size = img.pixel_array.shape
+        img_size = np.asarray(img_size).astype(float)
+
+        x_values = row.iloc[3:29:2].values 
+        y_values = row.iloc[4:29:2].values
+
+        # Combine x and y values and filter out NaN pairs
+        xy_pairs = np.array(list(zip(x_values, y_values)))
+
+        masks, scores, _ = predictor.predict(
+            point_coords=xy_pairs,
+            point_labels=None,
+            box=None,
+            multimask_output=False,
+        )
+
+        mask_sum = masks[0,0,:,:]
+        for mask in masks:
+            mask_sum += mask
+
+        plt.imshow(mask_sum, cmap='gray')
+        plt.savefig(os.path.join(output_dir,image_name+".png"))
+        plt.close()        
+        
+
+if __name__ == '__main__':
+
+    main()
